48f971b75efec9f9eee694067c647a2e458a911e
mgallegoa <manuelgallego2003@yahoo.com>
2026-02-10 22:25:08 -0500

    KAFKA: Spring boot sckeleton project with the modules Producer and Consumer.

---
+++ kafkaProducer/src/test/java/com/co/manuel/kafkaProducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.manuel.kafkaProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
